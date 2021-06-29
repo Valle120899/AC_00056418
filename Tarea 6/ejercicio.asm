babcd9037d339fc8304a3d30eee915f4fa165152
Valle120899 <54132073+Valle120899@users.noreply.github.com>
2021-06-28 22:53:16 -0600

    fixing code

--- Tarea 6/ejercicio.asm
+++ Tarea 6/ejercicio.asm
@@ -2,11 +2,7 @@
 
 	section	.text
 
-        ;print clave
-	mov     DX, ayuda
-        call    EscribirCadena
-        
-        ; print msg1
+	; print msg1
 	mov 	DX, msg1
 	call  	EscribirCadena
 
@@ -14,8 +10,6 @@
 	mov 	BP, frase
 	call  	LeerCadena
 
-        ;Verificacion
-        ;call Verificar
 
 	call	EsperarTecla
 
@@ -23,12 +17,11 @@
 
 	section	.data
 
-ayuda   db      "La clave es: abcde$"
-msg1	db	"Ingresa una clave: ", "$"
-clave   db      "abcde"
-msg3    db      "Bienvenido"
-msg4    db      "Incorrecto"
+msg1	db	"Type the password: ", "$"
 frase 	times 	20  	db	" " 	
+msg2    db      "Bienvenido"
+msg3    db      "Incorrecto"
+password   db      "abcde"
 
 ; FUNCIONES
 
@@ -36,35 +29,27 @@ EsperarTecla:
         mov     AH, 01h         
         int     21h
         ret
+
+; Leer cadena de texto desde el teclado
+; Salida:       SI: longitud de la cadena 		BP: direccion de guardado
 LeerCadena:
-        xor     SI, SI      
+        xor     SI, SI          ; SI = 0
 while:  
-        call    EsperarTecla    
-        cmp     AL, 0x0D      
-        je      exit            
-        mov     [BP+SI], AL   
-        inc     SI             
-        jmp     while          
+        call    EsperarTecla    ; retorna un caracter en AL
+        cmp     AL, 0x0D        ; comparar AL con caracter EnterKey
+        je      exit            ; si AL == EnterKey, saltar a exit
+        mov     [BP+SI], AL   	; guardar caracter en memoria
+        inc     SI              ; SI++
+        jmp     while           ; saltar a while
 exit:
-	mov 	byte [BP+SI], "$"	
+	mov 	byte [BP+SI], "$"	; agregar $ al final de la cadena
         ret
 
+
+; Permite escribir en la salida estándar una cadena de caracteres o string, este
+; debe tener como terminación el carácter “$”
+; Parámetros:	AH: 09h 	DX: dirección de la celda de memoria inicial de la cadena
 EscribirCadena:
 	mov 	AH, 09h
 	int 	21h
 	ret
-
-Verificar:
-        mov dx, clave
-        mov ax, frase
-        je Bueno
-        je Malo
-Bueno:
-        mov dx, msg3
-        call EscribirCadena
-        ret
-Malo:
-        mov dx, msg4
-        call EscribirCadena
-        ret
-;cmpsb
