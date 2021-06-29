79c6e0df3b68c7d54db01d3dc80d38cd71b80906
Valle120899 <54132073+Valle120899@users.noreply.github.com>
2021-06-28 23:33:03 -0600

    Terminado(?
    parcialmente

--- Tarea 6/ejercicio.asm
+++ Tarea 6/ejercicio.asm
@@ -1,4 +1,10 @@
-	org 	100h
+;IMPORTANTE: el programa funciona parcialmente.
+;La contraseña es "abcde", y la reconoce, pero 
+;cuando se dijita, si pone si está correcta o no
+;pero también imprime la otra frase seguida,
+; o al menos eso pasa en mi máquina
+;pero lo importante de ver si está correcta o no lo hace parcialmente c:
+        org 	100h
 
 	section	.text
 
@@ -10,18 +16,16 @@
 	mov 	BP, frase
 	call  	LeerCadena
 
+        mov     word cx, 5d
+        xor     DI, DI
+        jmp Comparar
+
 
 	call	EsperarTecla
 
 	int 	20h
 
-	section	.data
 
-msg1	db	"Type the password: ", "$"
-frase 	times 	20  	db	" " 	
-msg2    db      "Bienvenido"
-msg3    db      "Incorrecto"
-password   db      "abcde"
 
 ; FUNCIONES
 
@@ -53,3 +57,38 @@ EscribirCadena:
 	mov 	AH, 09h
 	int 	21h
 	ret
+
+Comparar:
+;Bp está guardado la frase colocada
+;[password + const] Puedo acceder a caracter de la contraseña     
+        mov     AX, [password+DI]
+        mov     BX, [frase+DI]
+        cmp     AX, BX
+        JNE     Malo
+        inc     DI
+        loop Comparar
+        jmp     Bueno
+
+Malo:
+        XOR     AX, AX
+        mov 	DX, msg3
+	jmp  	EscribirCadena
+        
+
+Bueno:
+        XOR     AX, AX
+        mov 	DX, msg2
+	jmp  	EscribirCadena
+        
+
+section	.data
+
+        password   db      "abcde"
+
+        msg1	db	"Type the password: ", "$"
+
+        msg3    db      "Incorrecto"
+
+        msg2    db      "Bienvenido"
+
+        frase 	times 	20  	db	" " 	
